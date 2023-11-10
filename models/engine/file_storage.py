ac8c6b2407628c2ac1d0fb78ce8d12e5b3ee11b8
raxidbou4nja <rachid.boughnja@gmail.com>
2023-11-10 19:58:26 +0000

    add task 3 4 files

---
+++ models/engine/file_storage.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import json
+from os.path import exists
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return FileStorage.__objects
+
+    def new(self, obj):
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        obj_dict = {}
+        for key, value in FileStorage.__objects.items():
+            obj_dict[key] = value.to_dict()
+        with open(FileStorage.__file_path, mode='w', encoding='utf-8') as file:
+            json.dump(obj_dict, file)
+
+    def reload(self):
+        if exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path, mode='r', encoding='utf-8') as file:
+                obj_dict = json.load(file)
+                for key, value in obj_dict.items():
+                    class_name, obj_id = key.split('.')
+                    obj_instance = eval(class_name)(**value)
+                    FileStorage.__objects[key] = obj_instance
