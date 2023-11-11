a3f3a04ee471ae0cf694c737bac927fd2ea66df3
raxidbou4nja <rachid.boughnja@gmail.com>
2023-11-11 14:52:24 +0000

    add python console

---
+++ console.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Console Module
+"""
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the program on EOF"""
+        print("")  # Print a new line for better formatting
+        return True
+
+    def emptyline(self):
+        """Do nothing on an empty line"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
